734361e1203c374009774457fef06a72aec5d483
ishy <ishy@163.com>
2022-12-09 23:05:47 +0800

    feat: 添加资源计算器功能

---
+++ src/components/dialog/calculate-dialog.vue
@@ -0,0 +1,225 @@
+<template>
+  <a-modal
+    v-model:visible="visible"
+    title="资源兑换计算"
+    cancelText="继续选择"
+    okText="关闭"
+    :width="950"
+    :closable="false"
+    :maskClosable="false"
+    @ok="handleOk"
+    @cancel="handleCancel"
+  >
+    粮食：<a-input-number v-model:value="resObj.food" :min="0" /> 木材：<a-input-number
+      v-model:value="resObj.wood"
+      :min="0"
+    />
+    钢铁：<a-input-number v-model:value="resObj.steel" :min="0" /> 汽油：<a-input-number
+      v-model:value="resObj.gas"
+      :min="0"
+    />
+    <span class="ml12">税率：</span>
+    <a-input-number
+      v-model:value="resObj.tax"
+      :formatter="(v) => `${v}%`"
+      :parser="(v) => v.replace('%', '')"
+      :min="0"
+      :max="100"
+    />
+    <a-button class="ml12" type="primary" @click="handleCalculate">计算</a-button>
+    <a-table
+      key="account"
+      class="mt12"
+      :scroll="{ x: true, y: '500px' }"
+      :dataSource="tableData"
+      :columns="columns"
+      :pagination="false"
+      @change="handlePageChange"
+    />
+  </a-modal>
+</template>
+<script lang="ts">
+import { defineComponent, ref, toRaw, reactive, watch } from "vue";
+export default defineComponent({
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+    selectedRows: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup(props, ctx) {
+    let selectDataList: any = [];
+    const visible = ref<boolean>(props.value);
+    const tableData: any = ref([]);
+    const columns = ref();
+    const resObj = reactive({
+      food: 0,
+      wood: 0,
+      steel: 0,
+      gas: 0,
+      tax: 25,
+    });
+    const fieldMap = {
+      food: "粮食",
+      wood: "木头",
+      steel: "铁矿",
+      gas: "金币",
+    };
+    const exchangeRate = {
+      food: 10,
+      wood: 10,
+      steel: 2,
+      gas: 0.5,
+    };
+
+    watch(
+      () => props.value,
+      (v) => {
+        visible.value = v;
+        if (v) {
+          selectDataList = [];
+          props.selectedRows.forEach((item) => {
+            // 把稀土比例化
+            let rowData: any = JSON.parse(JSON.stringify(toRaw(item)));
+            rowData["稀土"] = rowData["稀土"] * 200;
+            // 取出源数据
+            selectDataList.push(rowData);
+          });
+        }
+      }
+    );
+
+    /**
+     * 均衡策略算法
+     * 为每个窗口分发平均的资源数量
+     */
+    const handleCalculate = () => {
+      // 先计算扣税前的数值
+      const totalObj = {}; // 选中的账号的所有资源数量
+      const exchangeObj = {}; // 各个账号兑换资源的数量
+      const targetResObj = {}; // 各个资源需要兑换的数量
+      const resRawData = toRaw(resObj);
+
+      // 计算各个资源需要兑换的数量
+      const keyList = Object.keys(resRawData).slice(0, 4);
+      for (let key of keyList) {
+        // 小数向上保留
+        targetResObj[key] = Math.ceil((resRawData[key] / (100 - resRawData["tax"])) * 100);
+      }
+
+      // 计算选中的所有资源数量
+      keyList.forEach((k) => {
+        totalObj[k] = 0;
+      });
+      selectDataList.reduce((cur: any, next: any) => {
+        keyList.forEach((k) => {
+          cur[k] = Number((cur[k] + next[fieldMap[k]]).toFixed(2));
+        });
+        return cur;
+      }, totalObj);
+
+      // 对选中的数据根据稀土进行排序
+      selectDataList.sort((cur: Object, next: Object) => cur["稀土"] - next["稀土"]);
+
+      // 计算每个账号需要兑换的资源
+      for (let key in targetResObj) {
+        if (totalObj[key] < targetResObj[key]) {
+          selectDataList.forEach((item: Object, index: number) => {
+            // 计算低于平均分摊资源的账号数目
+            const targetAvg = (targetResObj[key] / (selectDataList.length - index)).toFixed(2);
+            let count = 0;
+            selectDataList.slice(index).forEach((item: Object) => {
+              if (item[fieldMap[key]] < targetAvg) {
+                count++;
+              }
+            });
+            // 均摊到每个少于平均值的账号上
+            const exchangeAvg = ((targetResObj[key] - totalObj[key]) / count).toFixed(2);
+            const targetExchangeNumber = Math.ceil((200 * Number(exchangeAvg)) / exchangeRate[key]);
+            // 超出稀土资源的逻辑处理
+            const actualExchangeNumber = item["稀土"] >= targetExchangeNumber ? targetExchangeNumber : item["稀土"];
+            exchangeObj[item["账号"]] = exchangeObj[item["账号"]] ?? {};
+            exchangeObj[item["账号"]][fieldMap[key] == "金币" ? "汽油" : fieldMap[key]] = actualExchangeNumber;
+            // 选中的账号的资源数量对应修改
+            item["稀土"] -= actualExchangeNumber;
+            item[fieldMap[key]] = Number(
+              (item[fieldMap[key]] + (actualExchangeNumber * exchangeRate[key]) / 200).toFixed(2)
+            );
+            // 总数对应修改
+            totalObj[key] = Number((totalObj[key] + (actualExchangeNumber * exchangeRate[key]) / 200).toFixed(2));
+          });
+        } else {
+          selectDataList.forEach((item: Object) => {
+            exchangeObj[item["账号"]] = exchangeObj[item["账号"]] ?? {};
+            exchangeObj[item["账号"]][fieldMap[key] == "金币" ? "汽油" : fieldMap[key]] = "-";
+          });
+        }
+      }
+
+      // 初始化表格数据
+      const accountList = Object.keys(exchangeObj);
+      tableData.value = accountList.map((account) => ({
+        账号: account,
+        ...exchangeObj[account],
+      }));
+      // 总计
+      const total = { 账号: "总计" };
+      for (let key in fieldMap) {
+        total[fieldMap[key] == "金币" ? "汽油" : fieldMap[key]] = `${totalObj[key]}(${
+          totalObj[key] >= targetResObj[key]
+            ? `足够,仅需${targetResObj[key]}`
+            : `还需${(targetResObj[key] - totalObj[key]).toFixed(2)}`
+        })`;
+      }
+      tableData.value.push(total);
+
+      const columnList = Object.keys(tableData.value[0]);
+      columns.value = columnList.map((v: any) => ({
+        title: v,
+        dataIndex: v,
+        width: 120,
+        resizable: true,
+      }));
+
+      console.log(accountList, tableData.value, exchangeObj);
+    };
+
+    /**
+     * 选择分页
+     */
+    const handlePageChange = (val: any) => {
+      pagination.value = val;
+    };
+
+    const handleOk = () => {
+      ctx.emit("update:value", false);
+    };
+
+    const handleCancel = () => {
+      ctx.emit("update:value", false);
+    };
+    return {
+      resObj,
+      visible,
+      columns,
+      tableData,
+      handleOk,
+      handleCancel,
+      handleCalculate,
+      handlePageChange,
+    };
+  },
+});
+</script>
+<style>
+.ml12 {
+  margin-left: 12px;
+}
+.mt12 {
+  margin-top: 12px;
+}
+</style>
--- src/components/excel-tabel.vue
+++ src/components/excel-tabel.vue
@@ -1,180 +1,235 @@
 <template>
-    <div class="common-card">
-        <div class="header">
-            <div>
-              账号序号：<a-input-number id="inputNumber" v-model:value="count" :min="0" />
-              <a-input v-model:value="account" style="width: 200px;margin-left: 16px;" placeholder="请输入账号"></a-input>
-              <a-button class="ml16" type="primary" @click="handleFilter">
-                  筛选
-              </a-button>
-              <a-button class="ml16" type="default" @click="handleReset">
-                  重置
-              </a-button>
-            </div>
-            <div>
-              <input ref="fileRef" type="file" name="file" webkitdirectory @change.stop="handleBatchImport" />
-              <a-button type="primary" style="margin-right: 16px" @click="resetData">
-                  清空数据
-              </a-button>
-              <!-- <a-button type="primary" @click="exportExcelFromTable">
+  <div class="common-card">
+    <div class="header">
+      <div>
+        账号序号：<a-input-number id="inputNumber" v-model:value="count" :min="0" />
+        <a-input v-model:value="account" style="width: 200px; margin-left: 16px" placeholder="请输入账号"></a-input>
+        <a-button class="ml16" type="primary" @click="handleFilter"> 筛选 </a-button>
+        <a-button class="ml16" type="default" @click="handleReset"> 重置 </a-button>
+        <a-button class="ml16" type="primary" @click="handleCalculate">
+          {{ btnName }}
+        </a-button>
+      </div>
+      <div>
+        <input ref="fileRef" type="file" name="file" webkitdirectory @change.stop="handleBatchImport" />
+        <a-button type="primary" style="margin-right: 16px" @click="resetData"> 清空数据 </a-button>
+        <!-- <a-button type="primary" @click="exportExcelFromTable">
                   导出Excel
               </a-button> -->
-            </div>
-        </div>
-        <a-table :scroll="{x: true, y: '600px'}" :dataSource="excelList" :columns="columns" :pagination="pagination" @change="handlePageChange" />
+      </div>
     </div>
+    <a-table
+      key="账号"
+      :scroll="{ x: true, y: '600px' }"
+      :dataSource="excelList"
+      :columns="columns"
+      :pagination="pagination"
+      :row-selection="rowSelection"
+      @change="handlePageChange"
+    />
+  </div>
+  <CalculateDialog v-model:value="visible" :selectedRows="state.selectedRows" />
 </template>
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import { exportExcel, file2Excel } from '../utils/excel'
+import { defineComponent, ref, reactive, computed } from "vue";
+import { exportExcel, file2Excel } from "../utils/excel";
+
+import CalculateDialog from "./dialog/calculate-dialog.vue";
 
 export default defineComponent({
-    setup() {
-        const count = ref(0);
-        const account = ref();
-        const fileRef = ref();
-        const excelList = ref([])
-        let pkgExcelList: any[] = []
-        let promiseList: Array<any> = []
-        let exportData: (String[] | null)[] = []
-        const columns = ref();
-        const pagination = ref({
-            current: 1,
-            pageSize: 20
-        })
-        /**
-         * 导出
-         */
-        const exportExcelFromTable = () => {
-            exportExcel(exportData, 'sheet')
-        }
-        /**
-         * 清空数据
-         */
-        const resetData = () => {
-            count.value = 0
-            account.value = ''
-            excelList.value = []
-            promiseList = []
-            // 清除input选择的file
-            fileRef.value.value = ''
-        }
-        /**
-         * 批量导入
-         */
-        const handleBatchImport = () => {
-            const files = fileRef.value?.files
-            for(let file of files) {
-                if (file.name.includes('统计')) {
-                    promiseList.push(file2Excel(file))
-                }
-            }
-            Promise.all(promiseList).then(fileList => {
-                let list:any = []
-                fileList.forEach(item => {
-                    const length = item[0]?.data?.length
-                    list = list.concat(item[0]?.data?.slice(0,length-1))
-                })
-                const keyList = Object.keys(list[0])
-                const keyLength = keyList.length
-                columns.value = keyList.slice(0,keyLength - 1).map((v:any) => ({
-                    title: v,
-                    dataIndex: v,
-                    key: v,
-                    fixed: v === '用户名',
-                    width: 120,
-                    resizable: true,
-                    sorter: (a: any, b: any) => a[v] - b[v],
-                    sortDirections: ['ascend','descend'],
-                }))
-                pkgExcelList = [...list]
-                list = handleCalcTotal(list)
-                excelList.value = list
-            })
-        }
-        /**
-         * 统计
-         */
-        const handleCalcTotal: any = (list: Array<any>) => {
-          const keys = Object.keys(list[0]).slice(2, 9)
-          const totalObj = {}
-          
-          keys.forEach(k => {
-              totalObj[k] = k === '等级' ? '总计' : 0
-          }) 
-          list.reduce((cur: any,next: any) => {
-              keys.slice(1).forEach(k => {
-                  cur[k] = (Number(cur[k]) + next[k]).toFixed(2)
-              })
-              return cur
-          },totalObj)
-          list.unshift(totalObj)
-          return list
-        }
-        /**
-         * 筛选账号
-         */
-        const handleFilter = () => {
-          const list = pkgExcelList.filter((item: any) => {
-            if (Number(count.value)) {
-              return item.count == count.value
-            }
-            return true
-          }).filter((item: any) => {
-            if (account.value) {
-                return item['账号'] === account.value
-            }
-            return true
-          })
-          if (list.length) {
-            excelList.value = handleCalcTotal(list)
-          }else {
-            excelList.value = []
-          }
-        }
-        /**
-         * 重置账号数据
-         */
-        const handleReset = () => {
-          count.value = 0
-          account.value = ''
-          handleFilter()
-        }
-        /**
-         * 选择分页
-         */
-        const handlePageChange = (val: any) => {
-            pagination.value = val
-        }
+  components: {
+    CalculateDialog,
+  },
+  setup() {
+    const count = ref(0);
+    const account = ref();
+    const fileRef = ref();
+    const visible = ref(false);
+    const excelList = ref([]);
+    let pkgExcelList: any[] = [];
+    let promiseList: Array<any> = [];
+    let exportData: (String[] | null)[] = [];
+    const columns = ref();
+    const btnName = ref("资源计算器");
+    const state = reactive({
+      selectedRowKeys: [],
+      selectedRows: [],
+    });
+    const pagination = ref({
+      current: 1,
+      pageSize: 20,
+    });
+
+    // 计算属性
+
+    // 多选配置选项
+    const rowSelection = computed(() => {
+      if (btnName.value !== "资源计算器") {
         return {
-          count,
-          account,
-          excelList,
-          columns,
-          fileRef,
-          pagination,
-          resetData,
-          handleReset,
-          handleFilter,
-          handlePageChange,
-          handleBatchImport,
-          exportExcelFromTable
+          onChange: (selectedRowKeys: any, selectedRows: any) => {
+            state.selectedRowKeys = selectedRowKeys;
+            state.selectedRows = selectedRows;
+          },
+        };
+      }
+      return null;
+    });
+
+    // 方法
+
+    /**
+     * 导出
+     */
+    const exportExcelFromTable = () => {
+      exportExcel(exportData, "sheet");
+    };
+    /**
+     * 清空数据
+     */
+    const resetData = () => {
+      count.value = 0;
+      account.value = "";
+      excelList.value = [];
+      promiseList = [];
+      // 清除input选择的file
+      fileRef.value.value = "";
+    };
+    /**
+     * 批量导入
+     */
+    const handleBatchImport = () => {
+      const files = fileRef.value?.files;
+      for (let file of files) {
+        if (file.name.includes("统计")) {
+          promiseList.push(file2Excel(file));
         }
-    },
-})
+      }
+      Promise.all(promiseList).then((fileList) => {
+        let list: any = [];
+        fileList.forEach((item) => {
+          const length = item[0]?.data?.length;
+          list = list.concat(item[0]?.data?.slice(0, length - 1));
+        });
+        const keyList = Object.keys(list[0]);
+        const keyLength = keyList.length;
+        columns.value = keyList.slice(0, keyLength - 1).map((v: any) => ({
+          title: v == "金币" ? "汽油" : v,
+          dataIndex: v,
+          fixed: v === "用户名",
+          width: 120,
+          resizable: true,
+          sorter: (a: any, b: any) => a[v] - b[v],
+          sortDirections: ["ascend", "descend"],
+        }));
+        // 列表需要key
+        list.forEach((item: Object, index: Number) => {
+          item["key"] = "" + index;
+        });
+        pkgExcelList = [...list];
+        list = handleCalcTotal(list);
+        excelList.value = list;
+      });
+    };
+    /**
+     * 统计
+     */
+    const handleCalcTotal: any = (list: Array<any>) => {
+      const keys = Object.keys(list[0]).slice(2, 9);
+      const totalObj = {};
+
+      keys.forEach((k) => {
+        totalObj[k] = k === "等级" ? "总计" : 0;
+      });
+      list.reduce((cur: any, next: any) => {
+        keys.slice(1).forEach((k) => {
+          cur[k] = (Number(cur[k]) + next[k]).toFixed(2);
+        });
+        return cur;
+      }, totalObj);
+      list.unshift(totalObj);
+      return list;
+    };
+    /**
+     * 筛选账号
+     */
+    const handleFilter = () => {
+      const list = pkgExcelList
+        .filter((item: any) => {
+          if (Number(count.value)) {
+            return item.count == count.value;
+          }
+          return true;
+        })
+        .filter((item: any) => {
+          if (account.value) {
+            return item["账号"] === account.value;
+          }
+          return true;
+        });
+      if (list.length) {
+        excelList.value = handleCalcTotal(list);
+      } else {
+        excelList.value = [];
+      }
+    };
+    /**
+     * 重置账号数据
+     */
+    const handleReset = () => {
+      count.value = 0;
+      account.value = "";
+      handleFilter();
+    };
+    /**
+     * 选择分页
+     */
+    const handlePageChange = (val: any) => {
+      pagination.value = val;
+    };
+    /**
+     * 资源计算器
+     */
+    const handleCalculate = () => {
+      if (btnName.value == "资源计算器") {
+        btnName.value = "选择完毕";
+      } else {
+        visible.value = true;
+      }
+    };
+    return {
+      count,
+      state,
+      account,
+      excelList,
+      columns,
+      fileRef,
+      btnName,
+      visible,
+      pagination,
+      rowSelection,
+      resetData,
+      handleReset,
+      handleFilter,
+      handleCalculate,
+      handlePageChange,
+      handleBatchImport,
+      exportExcelFromTable,
+    };
+  },
+});
 </script>
 <style>
 .common-card {
-    width: 100%;
-    padding: 24px;
-    border-radius: 12px;
+  width: 100%;
+  padding: 24px;
+  border-radius: 12px;
 }
 .header {
-    display: flex;
-    justify-content: space-between;
-    height: 54px;
+  display: flex;
+  justify-content: space-between;
+  height: 54px;
 }
 .ml16 {
-    margin-left: 16px;
+  margin-left: 16px;
 }
 </style>
